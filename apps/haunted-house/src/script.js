52325b2b21512d90caa6a062fa5ed0f2bb99eae2
subikstha <subsatch@gmail.com>
2026-02-15 16:55:49 +0545

    Added house and graves

--- apps/haunted-house/src/script.js
+++ apps/haunted-house/src/script.js
@@ -18,20 +18,102 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+// Floor
+const floorAlphaTexture = textureLoader.load('./resources/floor/alpha.jpg')
+
+/**
  * House
  */
 
 // Temporary Sphere
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(1, 32, 32), new THREE.MeshStandardMaterial({ roughness: 0.7 }))
-scene.add(sphere)
+// scene.add(sphere)
 
 // Floor
 const floor = new THREE.Mesh(
     new THREE.PlaneGeometry(20, 20),
-    new THREE.MeshStandardMaterial()
+    new THREE.MeshStandardMaterial({
+        alphaMap: floorAlphaTexture, // after using alpha texture we need to inform threejs that it supports transparency,
+        transparent: true
+    })
 )
+floor.rotation.x = -Math.PI * .5
+
 scene.add(floor)
 
+// House Group
+const house = new THREE.Group()
+scene.add(house)
+
+// Walls
+const walls = new THREE.Mesh(
+    new THREE.BoxGeometry(4, 2.5, 4),
+    new THREE.MeshStandardMaterial()
+)
+walls.position.y += 1.25;
+house.add(walls)
+
+// Roof
+const roof = new THREE.Mesh(
+    new THREE.ConeGeometry(3.5, 1.5, 4),
+    new THREE.MeshStandardMaterial()
+)
+roof.position.y += 1.5 / 2 + 2.5;
+roof.rotation.y = Math.PI / 4
+house.add(roof)
+
+// Doors
+const door = new THREE.Mesh(
+    new THREE.PlaneGeometry(2.2, 2.2),
+    new THREE.MeshStandardMaterial({ color: 0x00ff00 })
+)
+door.position.z += 2 + 0.01;  // add this to prevent z fighting
+door.position.y += 1;
+house.add(door)
+
+// Bushes
+const bushGeometry = new THREE.SphereGeometry(1, 16, 16)
+const bushMaterial = new THREE.MeshStandardMaterial()
+
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush1.scale.setScalar(0.5)
+bush1.position.set(0.8, 0.2, 2.2)
+
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush2.scale.setScalar(0.25)
+bush2.position.set(1.4, 0.1, 2.1)
+
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush3.scale.setScalar(0.4)
+bush3.position.set(-.8, 0.1, 2.2)
+
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush4.scale.setScalar(0.15)
+bush4.position.set(-1, 0.05, 2.6)
+house.add(bush1, bush2, bush3, bush4)
+
+// Graves
+const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2)
+const graveMaterial = new THREE.MeshStandardMaterial()
+const graves = new THREE.Group()
+scene.add(graves)
+
+for (let i = 0; i < 30; i++) {
+    const angle = Math.random() * Math.PI * 2
+    const radius = 3 + Math.random() * 4;
+    const x = Math.sin(angle) * radius;
+    const z = Math.cos(angle) * radius;
+
+    const grave = new THREE.Mesh(graveGeometry, graveMaterial)
+    grave.rotation.x = (Math.random() - .5) * 0.4
+    grave.rotation.y = (Math.random() - .5) * 0.4
+    grave.rotation.z = (Math.random() - .5) * 0.4
+    grave.position.set(x, Math.random() * 0.4, z)
+    graves.add(grave)
+}
 /**
  * Lights
  */
@@ -78,6 +160,9 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 
 camera.position.x = 4
 camera.position.y = 2
 camera.position.z = 5
+// camera.position.x = 1
+// camera.position.y = 1
+// camera.position.z = 5
 scene.add(camera)
 
 // Controls
